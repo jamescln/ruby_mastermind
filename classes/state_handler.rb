511bda1b0d1e7f4d0ee22d44d5eaa3bb919ae654
James <james.jcarline@gmail.com>
2022-12-17 12:02:05 +0000

    Add StateHandler class

---
+++ classes/state_handler.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# StateHandler will, unbelievably, handle the state of the game
+class StateHandler
+  # set initial values for new instance
+  def initialize(name)
+    @pl_name = name
+  end
+end
