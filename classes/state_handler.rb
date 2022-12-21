9b6ff0efbeaf71d9787e43a7d025f7d2582de917
James <james.jcarline@gmail.com>
2022-12-21 14:30:15 +0000

    Add create_code method to StateHandler

--- classes/state_handler.rb
+++ classes/state_handler.rb
@@ -5,5 +5,12 @@ class StateHandler
   # set initial values for new instance
   def initialize(name)
     @pl_name = name
+    @colors = %w[red blue yellow green purple orange]
+    @code = ['', '', '', '']
+  end
+
+  # create a random code
+  def create_code
+    @code.fill(@colors.rand(6))
   end
 end
